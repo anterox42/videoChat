eed6a9097dd6e6fbaf0ebf26283f06d8255dd2f7
Andrey Tytenko <andrey.tytenko@deepxhub.com>
2021-04-25 13:28:51 +0300

    emotion detection

--- main.js
+++ main.js
@@ -3,6 +3,10 @@ import './style.css';
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 
+// import faceapi from '/models/faceapi.nets.tinyFaceDetector';
+
+// faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
+
 const firebaseConfig = {
   apiKey: 'AIzaSyB5ING7YtioHyIRpU5VzGexPydIFhsG09E',
   authDomain: 'webrtc-18756.firebaseapp.com',
@@ -41,6 +45,13 @@ const remoteVideo = document.getElementById('remoteVideo');
 const hangupButton = document.getElementById('hangupButton');
 const callTextId = document.getElementById('callTextId');
 
+Promise.all([
+  faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
+  faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+  // faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
+  faceapi.nets.faceExpressionNet.loadFromUri('/models'),
+]);
+
 // 1. Setup media sources
 
 webcamButton.onclick = async () => {
@@ -64,6 +75,52 @@ webcamButton.onclick = async () => {
 
   webcamVideo.srcObject = localStream;
   remoteVideo.srcObject = remoteStream;
+
+  // webcamVideo.addEventListener('playing', () => {
+  //   const canvas = faceapi.createCanvasFromMedia(webcamVideo);
+  //   document.body.append(canvas);
+  //   const displaySize = {
+  //     width: webcamVideo.width,
+  //     height: webcamVideo.height,
+  //   };
+  //   faceapi.matchDimensions(canvas, displaySize);
+  //   setInterval(async () => {
+  //     const detections = await faceapi
+  //       .detectAllFaces(webcamVideo, new faceapi.TinyFaceDetectorOptions())
+  //       .withFaceLandmarks()
+  //       .withFaceExpressions();
+  //     const resizedDetections = faceapi.resizeResults(detections, displaySize);
+  //     canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
+  //     faceapi.draw.drawDetections(canvas, resizedDetections);
+  //     faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
+  //     faceapi.draw.drawFaceExpressions(canvas, resizedDetections);
+  //   }, 1000);
+
+  //   console.log('listener added to webcam video');
+  // });
+
+  remoteVideo.addEventListener('playing', () => {
+    const canvas = faceapi.createCanvasFromMedia(remoteVideo);
+    document.body.append(canvas);
+    const displaySize = {
+      width: remoteVideo.width,
+      height: remoteVideo.height,
+    };
+    faceapi.matchDimensions(canvas, displaySize);
+    setInterval(async () => {
+      const detections = await faceapi
+        .detectAllFaces(remoteVideo, new faceapi.TinyFaceDetectorOptions())
+        .withFaceLandmarks()
+        .withFaceExpressions();
+      const resizedDetections = faceapi.resizeResults(detections, displaySize);
+      canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
+      faceapi.draw.drawDetections(canvas, resizedDetections);
+      faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
+      faceapi.draw.drawFaceExpressions(canvas, resizedDetections);
+    }, 1000);
+
+    console.log('listener added to remote video');
+  });
 
   callButton.disabled = false;
   answerButton.disabled = false;
